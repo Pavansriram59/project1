aedf05fb1ebb1b9df3e17fe0e7437ec7e831792f
Pavansriram <95296797+Pavansriram59@users.noreply.github.com>
2026-01-31 22:03:48 +0530

    code for sum of 2 nums

---
+++ test.py
@@ -0,0 +1,8 @@
+#generate code for sum of two numbers
+def sum_of_two_numbers(a, b):
+    return a + b        
+# Example usage
+result = sum_of_two_numbers(3, 5)
+print("The sum is:", result)
+print("Hello, World!")
+
